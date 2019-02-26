7fcd2e0f80326739244f97f821d75bbc2080a0a4
Dat Le <datlequoc96@gmail.com>
2019-02-26 13:50:57 +0200

    Bootstrap & Navbar

---
+++ application/views/front_page/nav_bar.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="application/assets/boostrap/css/bootstrap.css">
+<script src="application/assets/boostrap/js/bootstrap.min.js" ></script>
+</head>
+<body>
+  <header>
+    <!-- <nav class="navbar navbar-expand-lg" style="background-color:#050D3B;">
+    <div class="col-6" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="#"><img src="application/assets/images/white_logo.svg" height="50%" width="50%" class="d-inline-block align-top" alt=""></a>
+    </div>
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+          <ul class="nav navbar-nav navbar-left">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item text-left">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item text-left">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav> -->
+      <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#050D3B;">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <a class="navbar-brand" href="#"><img src="application/assets/images/white_logo.svg" height="50%" width="50%" class="d-inline-block align-top" alt=""></a>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">PayB</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Whitepayper</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+  </header>
+</body>
+</html>
